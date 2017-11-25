64f54cd6abbfad96809f8b38165d8b1bcaad2f06
Jakub Fajkus <jakub.fajkus@gmail.com>
2017-11-25 13:51:11 +0100

    separator funkcni bez vstupu

--- main.cc
+++ main.cc
@@ -4,7 +4,8 @@
 #define T_PM 10
 #define T_BUFFER 12
 #define T_METAL_SHEET_GENERATOR 300
-#define C_SHEET_METAL_PALET 10
+
+#define C_SHEET_METAL_PALET 2
 
 Queue pmQueue("pmQueue");
 Facility pm("Punching machine", pmQueue);
@@ -18,14 +19,64 @@ Facility pmLoadingBuffer("pmLoadingBuffer", pmLoadingBufferQueue);
 Queue pmPreQueue("pmPreQueue");
 Facility pmPreWorker("pmPreWorker", pmPreQueue);
 
-Store processedSheetMetalCounter("Sheet metal counter", C_SHEET_METAL_PALET);
+Queue separatorQueue("separatorQueue");
+Facility separator("separator", separatorQueue);
 
-Queue emptyPalletQueue("emptyPalletQueue");
+Store processedSheetMetalBufferCounter("processedSheetMetalBufferCounter", C_SHEET_METAL_PALET);
+
+Store hasPAndSPalet("hasPAndSPalet", 2);
+Store canLoadNextSkeletonPallet("canLoadNextSkeletonPallet", 1); //chceme defaultne 1!
+Store canLoadNextResultPallet("canLoadNextResultPallet", 1); //chceme defaultne 1!
+
+Store skeletonStack("skeletonStack", C_SHEET_METAL_PALET);
+Store resultStack("resultStack", C_SHEET_METAL_PALET);
+
+Queue emptyPalletQueue("emptyPalletQueue"); //todo: temp
 
 class EmptyPallet : public Process {
     void Behavior() {
         //todo: paleta nekam jde
         emptyPalletQueue.Insert(this);
+    }
+};
+
+
+class SkeletonPallet : public Process {
+
+private:
+    void Behavior() {
+        //pokud je stack plny, vyprazdnime ho a muzeme odvest paletu
+        if (skeletonStack.Empty()) {
+            skeletonStack.Enter(this, C_SHEET_METAL_PALET);
+//            canLoadNextSkeletonPallet.Leave(1); //todo: odkomentovat az budou vstupy
+            //todo: paleta nekam musi jit
+
+            //temp : nasimulujeme vytvoreni nove palety!!
+            (new SkeletonPallet())->Activate();
+        } else {
+            hasPAndSPalet.Leave(1);
+            return; //RIP palet
+        }
+    }
+};
+
+
+class ResultPallet : public Process {
+
+private:
+    void Behavior() {
+        //pokud je stack plny, vyprazdnime ho a muzeme odvest paletu
+        if (resultStack.Empty()) {
+            resultStack.Enter(this, C_SHEET_METAL_PALET);
+//            canLoadNextResultPallet.Leave(1); //todo: odkomentovat az budou vstupy
+            //todo: paleta nekam musi jit
+
+            //temp: nasimulujeme vytvoreni nove palety!!
+            (new ResultPallet())->Activate();
+        } else {
+            hasPAndSPalet.Leave(1);
+            return; //RIP palet
+        }
     }
 };
 
@@ -41,18 +92,35 @@ class MetalSheet : public Process {
         //zabereme PM
         this->Seize(pm);
 
-        //vlozime plech do counteru
-        processedSheetMetalCounter.Leave(1);
+        //vlozime plech do counteru bufferu
+        processedSheetMetalBufferCounter.Leave(1);
 
         //uvonime pre workera
         this->Release(pmPreWorker);
         this->Wait(T_PM);
         this->Release(pm);
+
+        //zabereme separator
+        this->Seize(separator);
+        //pockame, az budeme mit k dispozici prazdne paletu pro resulty a skeletony
+        hasPAndSPalet.Enter(this, 2);
+
+        //vlozime resulty a skeletony do bufferu
+        skeletonStack.Leave(1);
+        resultStack.Leave(1);
+
+        //vytvorime procesy pro paletu se skeletony a resulty
+        (new SkeletonPallet())->Activate();
+        (new ResultPallet())->Activate();
+
+        this->Release(separator);
+
+        return;//RIP plech
     }
 };
 
 
-class SheetMetalPallet : public Process {
+class MetalSheetPallet : public Process {
 
 private:
     void Behavior() {
@@ -63,27 +131,29 @@ private:
         }
 
         //pockame, nez se zpracuji vsechny plechy
-        processedSheetMetalCounter.Enter(this, C_SHEET_METAL_PALET);
+        processedSheetMetalBufferCounter.Enter(this, C_SHEET_METAL_PALET);
 
         (new EmptyPallet())->Activate();
         this->Release(pmBuffer);
+
+        return; //RIP paleta
     }
 };
 
 
-class SheetMetalPalletGenerator : public Event {
+class MetalSheetPalletGenerator : public Event {
 public:
     double Interval;
     bool did = false;
 
-    SheetMetalPalletGenerator(double interv) : Event() {
+    MetalSheetPalletGenerator(double interv) : Event() {
         Interval = interv;
     };
 
     void Behavior() {
         //vybereme sklad, protoze ho potrebujeme prazdny
 
-        (new SheetMetalPallet())->Activate();
+        (new MetalSheetPallet())->Activate();
         Activate(Time + Exponential(Interval));
     }
 };
@@ -93,16 +163,26 @@ public:
     bool did = false;
 
     void Behavior() {
-        //vybereme sklad, protoze ho potrebujeme prazdny
         if (!did) {
-            processedSheetMetalCounter.Enter(this, C_SHEET_METAL_PALET);
-            did=true;
+            //vybereme sklady, protoze je potrebujeme prazdne
+            processedSheetMetalBufferCounter.Enter(this, C_SHEET_METAL_PALET);
+//            hasPAndSPalet.Enter(this, 2);
+            skeletonStack.Enter(this, C_SHEET_METAL_PALET);
+            resultStack.Enter(this, C_SHEET_METAL_PALET);
+            canLoadNextSkeletonPallet.Enter(this, 1);
+            canLoadNextResultPallet.Enter(this, 1);
+
+            //todo: tmp!
+//            (new SkeletonPallet())->Activate();
+//            (new ResultPallet())->Activate();
+
+
+            did = true;
         }
 
-        (new SheetMetalPalletGenerator(T_METAL_SHEET_GENERATOR))->Activate();
+        (new MetalSheetPalletGenerator(T_METAL_SHEET_GENERATOR))->Activate();
     }
 };
-
 
 
 int main() {
@@ -127,6 +207,15 @@ int main() {
     pm.Output();
     pmQueue.Output();
 
-    processedSheetMetalCounter.Output();
+    processedSheetMetalBufferCounter.Output();
     emptyPalletQueue.Output();
+
+    separator.Output();
+    separatorQueue.Output();
+
+    hasPAndSPalet.Output();
+    canLoadNextSkeletonPallet.Output();
+    canLoadNextResultPallet.Output();
+    skeletonStack.Output();
+    resultStack.Output();
 }
